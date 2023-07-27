5c0e4257723cb3ee90362681ecabe9b05acc3236
Vladislavinho <anuvla11032003@mail.ru>
2023-07-27 20:39:43 +0300

    Commit my example.

--- cypress/e2e/Task2TimeTrack.cy.js
+++ cypress/e2e/Task2TimeTrack.cy.js
@@ -1,29 +1,61 @@
-describe('Issue comments creating, editing and deleting', () => {
+describe('Issue create', () => {
     beforeEach(() => {
         cy.visit('/');
         cy.url().should('eq', `${Cypress.env('baseUrl')}project/board`).then((url) => {
-            cy.visit(url + '/board');
-            cy.contains('This is an issue of type: Task.').click();
+            cy.visit(url + '/board?modal-issue-create=true');
         });
     });
+    const getIssueCreateModal = () => cy.get('[data-testid="modal:issue-create"]');
+    it('My task 1 creating new issue', () => {
+    
+    getIssueCreateModal().within(() => {
+     cy.get('[data-testid="select:type"]').click();
+     cy.get('.sc-iqzUVk.cUBVJX').contains('Bug')
+    .trigger('click');
+            
+     cy.get('input[name="title"]').click();
+     cy.get('input[name="title"]').type('Vlad')
+    .trigger('click');
+         
+    cy.get('button[type="submit"]').click();
+        })
+    cy.get('[data-testid="modal:issue-create"]').should('not.exist');
+    cy.contains('Issue has been successfully created.').should('be.visible');
+
+       
+    cy.reload();
+    cy.contains('Issue has been successfully created.').should('not.exist');
+
+        
+    cy.get('[data-testid="board-list:backlog').should('be.visible').and('have.length', '1').within(() => {
+           
+     cy.get('[data-testid="list-issue"]')
+    .should('have.length', '5')
+    .first()
+    .find('p')
+    .contains('Vlad')
+    .click()
+
+})
+})
     const getIssueDetailsModal = () => cy.get('[data-testid="modal:issue-details"]');
-    it.only('Should create a comment successfully', () => {
+    it('Estimation Task', () => {
         const lionelmessi = '10';
         const penaldo = '7';
         const NumberPlaceHolder = cy.get('input[placeholder="Number"]');
-        getIssueDetailsModal().within(() => {
-       //Write estimation
-       NumberPlaceHolder.click().clear()
-       NumberPlaceHolder.type(lionelmessi);
-       cy.get('input[placeholder="Number"]').should('have.value', lionelmessi);
-       //Edit estimation
-       NumberPlaceHolder.click().clear()
-       NumberPlaceHolder.type(penaldo);
-       cy.get('input[placeholder="Number"]').should('have.value', penaldo);
-       //Remove estimation
-       NumberPlaceHolder.click().clear()
-       cy.get('input[placeholder="Number"]').should('have.attr', 'value', '');   
-    })
-    })
 
+        getIssueDetailsModal().within(() => {
+            //Write estimation
+            NumberPlaceHolder.click().clear()
+            NumberPlaceHolder.type(lionelmessi);
+            cy.get('input[placeholder="Number"]').should('have.value', lionelmessi);
+            //Edit estimation
+            NumberPlaceHolder.click().clear()
+            NumberPlaceHolder.type(penaldo);
+            cy.get('input[placeholder="Number"]').should('have.value', penaldo);
+            //Remove estimation
+            NumberPlaceHolder.click().clear()
+            cy.get('input[placeholder="Number"]').should('have.attr', 'value', '');
+        })
+    })
 })
